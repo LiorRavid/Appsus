a8ecc611f51892137a51ad0a1837eb1ab8561b7f
Or Baadani <orbaadani@gmail.com>
2021-11-13 22:47:54 +0200

    img

--- js/apps/book/pages/book-app.cmp.js
+++ js/apps/book/pages/book-app.cmp.js
@@ -2,7 +2,8 @@ export default {
     template: `
        <section class="book-app flex-grow">
 
-<img src="../../../../img/under-construction.jpg" alt="">
+<!-- <img src="../../../../img/under-construction.jpg" alt=""> -->
+<img src="https://icon-library.com/images/under-construction-icon/under-construction-icon-1.jpg" alt="">
        </section>
     `,
 }
